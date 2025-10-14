9b74737a34d8102951812d95e2e1848e31e04af4
Pradixx <dieguittosilva09@gmail.com>
2025-10-13 23:01:06 -0300

    Evolução em Entitys

--- src/main/java/com/deigo/SimuladorDeInvestimentos/infrastructure/entitys/Investimentos.java
+++ src/main/java/com/deigo/SimuladorDeInvestimentos/infrastructure/entitys/Investimentos.java
@@ -1,22 +1,38 @@
 package com.deigo.SimuladorDeInvestimentos.infrastructure.entitys;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "investimentos")
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_investimento")
 
 public abstract class Investimentos {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "valor-inicial")
     private double valorInicial;
 
+    @Column(name = "taxa-juros")
     private double taxaJuros;
 
+    @Column(name = "periodo")
     private int periodo;
+
+    public abstract BigDecimal calcularRendimento ();
 }
