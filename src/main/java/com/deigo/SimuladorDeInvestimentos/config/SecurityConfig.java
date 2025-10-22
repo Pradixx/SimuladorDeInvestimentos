9b68dc4312efaf392dd0dd5167b24b8c64f2a302
Pradixx <dieguittosilva09@gmail.com>
2025-10-22 10:38:37 -0300

    Integrando Swagger

--- src/main/java/com/deigo/SimuladorDeInvestimentos/config/SecurityConfig.java
+++ src/main/java/com/deigo/SimuladorDeInvestimentos/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.deigo.SimuladorDeInvestimentos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -13,9 +15,12 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
+
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/investimentos/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
