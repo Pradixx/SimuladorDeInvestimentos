641306facb5525c9369ab93a29cc142a060252bb
Pradixx <dieguittosilva09@gmail.com>
2025-10-25 19:52:23 -0300

    Organizando o terreno

--- SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/investimentos/investimentos.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-investimentos',
-  standalone: true,
-  templateUrl: './investimentos.component.html',
-  styleUrls: ['./investimentos.component.css']
-})
-export class InvestimentosComponent { }
