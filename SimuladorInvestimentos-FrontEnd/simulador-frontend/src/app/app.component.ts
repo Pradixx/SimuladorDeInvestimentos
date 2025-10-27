97c120189d0760f7d72c78eebaadc68288556a34
Pradixx <dieguittosilva09@gmail.com>
2025-10-26 21:53:30 -0300

    Configurando

--- SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/app.component.ts
+++ SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-import { InvestimentosComponent } from './investimentos/investimentos.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [InvestimentosComponent],
-  template: `<app-investimentos></app-investimentos>`
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Simulador de Investimentos';
+  autor = 'Diego Prado'; 
+
+  constructor(private router: Router) {}
+
+  public navegarParaRegistrarInvestimento(): void {
+    console.log('Navegando para Registro de Investimento...');
+    this.router.navigate(['/registrar-investimento']);
+  }
+
+  public navegarParaCalcularRendimento(): void {
+    console.log('Navegando para Cálculo de Rendimento...');
+    this.router.navigate(['/calcular-rendimento']);
+  }
+
+  public navegarParaTodosInvestimentos(): void {
+    console.log('Navegando para Todos Investimentos...');
+    this.router.navigate(['/lista-investimentos']);
+  }
+}
--- SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/app.routes.ts
+++ SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CalcularRendimentoComponent } from './components/calcular-rendimento/calcular-rendimento';
+import { RegistrarInvestimentoComponent } from './components/registrar-investimento/registrar-investimento.component';
+import { ListaInvestimentosComponent } from './components/lista-investimentos/lista-investimentos.component'; // <-- Importe o novo componente
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'registrar-investimento',
+    component: RegistrarInvestimentoComponent,
+    title: 'Registrar Investimento',
+  },
+  {
+    path: 'calcular-rendimento',
+    component: CalcularRendimentoComponent,
+    title: 'Calcular Rendimento',
+  },
+  {
+    path: 'lista-investimentos', // Rota usada pelo botão do dashboard
+    component: ListaInvestimentosComponent, // <-- Componente de listagem
+    title: 'Todos Investimentos',
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/' }, 
+];
