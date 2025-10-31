187e799b4e1ade036c5cf93d85d3b5d26af424c9
Pradixx <dieguittosilva09@gmail.com>
2025-10-31 10:10:56 -0300

    Reescrevendo APP

--- SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/app.routes.ts
+++ SimuladorInvestimentos-FrontEnd/simulador-frontend/src/app/app.routes.ts
@@ -1,24 +1,9 @@
 import { Routes } from '@angular/router';
-import { CalcularRendimentoComponent } from './components/calcular-rendimento/calcular-rendimento';
-import { RegistrarInvestimentoComponent } from './components/registrar-investimento/registrar-investimento.component';
-import { ListaInvestimentosComponent } from './components/lista-investimentos/lista-investimentos.component'; // <-- Importe o novo componente
+import { Home } from './components/home/home';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
-  {
-    path: 'registrar-investimento',
-    component: RegistrarInvestimentoComponent,
-    title: 'Registrar Investimento',
-  },
-  {
-    path: 'calcular-rendimento',
-    component: CalcularRendimentoComponent,
-    title: 'Calcular Rendimento',
-  },
-  {
-    path: 'lista-investimentos', // Rota usada pelo botão do dashboard
-    component: ListaInvestimentosComponent, // <-- Componente de listagem
-    title: 'Todos Investimentos',
-  },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '**', redirectTo: '/' }, 
+  { path: '', component: Home },
+  { path: 'dashboard', component: Dashboard },
+  { path: '**', redirectTo: '' }
 ];
