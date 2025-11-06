9627908afaf22875d8369f7f1d674f04f5b94920
Pradixx <dieguittosilva09@gmail.com>
2025-11-06 14:06:27 -0300

    Resolvendo problema com botoes

--- SimuladorInvestimentos-FrontEnd/simulador-frontend/src/main.ts
+++ SimuladorInvestimentos-FrontEnd/simulador-frontend/src/main.ts
@@ -1,6 +1,15 @@
-
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideClientHydration() 
+  ]
+}).catch(err => console.error(err));
