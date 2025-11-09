125232f5bf094a9e8797048723375b3098990fea
Pradixx <dieguittosilva09@gmail.com>
2025-11-09 02:51:08 -0300

    Adicionando novas entitys para o calculo

--- SimuladorInvestimentos-BackEnd/src/main/java/com/deigo/SimuladorDeInvestimentos/controller/DTO/CriarInvestimentosDTO.java
+++ SimuladorInvestimentos-BackEnd/src/main/java/com/deigo/SimuladorDeInvestimentos/controller/DTO/CriarInvestimentosDTO.java
@@ -1,4 +1,13 @@
 package com.deigo.SimuladorDeInvestimentos.controller.DTO;
 
-public record CriarInvestimentosDTO(String tipo, String nome, Double valorInicial, Double taxaJuros, Integer periodo) {
+public record CriarInvestimentosDTO(String tipo,
+                                    String nome,
+                                    Double valorInicial,
+                                    Double taxaJuros,
+                                    Integer periodo,
+                                    Double taxaPrefixada,
+                                    Double ipca,
+                                    Double cdiPercentual,
+                                    Double selicAnual
+) {
 }
