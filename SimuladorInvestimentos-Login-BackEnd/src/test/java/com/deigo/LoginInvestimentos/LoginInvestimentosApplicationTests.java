948f44e129ac538a42a8fcf20b17ed5f1de0bb74
Pradixx <dieguittosilva09@gmail.com>
2025-11-18 11:32:54 -0300

    Innitial commit Login

---
+++ SimuladorInvestimentos-Login-BackEnd/src/test/java/com/deigo/LoginInvestimentos/LoginInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deigo.LoginInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
