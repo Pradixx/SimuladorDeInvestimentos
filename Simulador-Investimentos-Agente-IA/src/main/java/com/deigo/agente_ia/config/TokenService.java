dfd502c5ccaf0de90f717f3b2e755b4ce9cb18e0
Pradixx <dieguittosilva09@gmail.com>
2025-11-23 22:47:31 -0300

    Configurações de segurança Agente IA

---
+++ Simulador-Investimentos-Agente-IA/src/main/java/com/deigo/agente_ia/config/TokenService.java
@@ -0,0 +1,34 @@
+package com.deigo.agente_ia.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${security.jwt.secret-key}")
+    private String secret;
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
